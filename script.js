14240aaea7b81b01449c0b029eb8ba427f497f33
Alan Nguyen <tuan8101@gmail.com>
2021-01-13 19:32:58 -0700

    Create toggleVideoStatus

--- script.js
+++ script.js
@@ -4,6 +4,35 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
+
+// Update progress & timestamp
+function updateProgess() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  return true;
+}
+
 // Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
